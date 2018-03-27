a95b346fa81bdbf4d42e0390ad00b0f1845c48ad
DirectSoftAgency <36406999+DirectSoftAgency@users.noreply.github.com>
2018-03-27 15:44:33 +0200

    Add files via upload

---
+++ places.js
@@ -0,0 +1,20 @@
+export const Places =
+  [
+    { "name": "Sandton City", "lat": -26.108260, "long": 28.053138, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/ArtsCentre.jpg" },
+    { "name": "Nelson Mandela Square", "lat": -26.107404, "long": 28.054727, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/CrownCasino.jpg" },
+    { "name": "Sandton Convention Centre", "lat": -26.106068, "long": 28.053188, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/EurekaSkydeck.jpg" },
+    { "name": "Johannesburg Stock Exchange", "lat": -26.103349, "long": 28.057278, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/FederationSquare.jpg" },
+    { "name": "Mushroom Park", "lat": -26.104765, "long": 28.062084, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/MCG.jpg" },
+    { "name": "Atholl Square", "lat": -26.106312, "long": 28.066834, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/MelbourneZoo.jpg" },
+    { "name": "The Sands", "lat": -26.111894, "long": 28.058845, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/OldMelbourneGaol.jpg" },
+    { "name": "Hilton Hotel - Sandton", "lat": -26.101493, "long": 28.059935, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/BotanicGardens.jpg" },
+    { "name": "Gautrain Station - Sandton", "lat": -26.107867, "long": 28.057285, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/ShrineOfRemembrance.jpg" },
+    { "name": "The Maslow - Sandton", "lat": -26.098733, "long": 28.058985, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/StPatricks.jpg" },
+    { "name": "The Capital Moloko", "lat": -26.097872, "long": 28.069353, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/StPauls.jpg" },
+    { "name": "Radisson Blu Hotel - Sandton", "lat": -26.104139, "long": 28.061375, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/BotanicGardens.jpg" },
+    { "name": "News Cafe, Sandton", "lat": -26.101759, "long": 28.056742, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/ShrineOfRemembrance.jpg" },
+    { "name": "McDonalds - Morningside", "lat": -26.097875, "long": 28.057121, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/StPatricks.jpg" },
+    { "name": "Sandton Police Station", "lat": -26.080262, "long": 28.061638, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/StPauls.jpg" },
+    { "name": "MediClinic - Sandton", "lat": -26.077726, "long": 28.012618, "image": "https://teamtamdemo.blob.core.windows.net/melbourne/StPauls.jpg" }
+    
+  ]
